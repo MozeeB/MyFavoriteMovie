8ab692ed6a98e1a827b987de96b17ea18b37b2dd
mujib <mujiburr741@gmail.com>
2019-08-14 14:43:53 +0700

    content provider done!

--- app/src/main/java/com/zeeb/myfavoritemovie/MainActivity.java
+++ app/src/main/java/com/zeeb/myfavoritemovie/MainActivity.java
@@ -1,6 +1,7 @@
 package com.zeeb.myfavoritemovie;
 
 import android.content.CursorLoader;
+import android.content.Intent;
 import android.content.Loader;
 import android.content.UriMatcher;
 import android.database.Cursor;
@@ -13,6 +14,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -92,6 +94,7 @@ public class MainActivity extends AppCompatActivity {
                 Glide.with(getApplicationContext())
                         .load(BuildConfig.URLIMAGE + cursor.getString(cursor.getColumnIndexOrThrow("poster_path")))
                         .into(holder.imgPoster);
+
             }
         }
 
